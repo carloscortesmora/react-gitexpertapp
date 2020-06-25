77c62004b207b3dcc16a8795adf8e6f58b5779ce
Carlos Cortes <carloscortesmora@gmail.com>
2020-06-25 00:35:24 -0400

    Se agrega carpeta docs

---
+++ docs/precache-manifest.86a31f750b0d1f4c9f72b05b4d4c1b29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1e1de9282f38b2c23da3f425b11bdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e28796b98a2ce29d3673",
+    "url": "/static/css/main.090469c5.chunk.css"
+  },
+  {
+    "revision": "93c7de5e2a4978d59147",
+    "url": "/static/js/2.69a29cce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.69a29cce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e28796b98a2ce29d3673",
+    "url": "/static/js/main.661527ff.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
